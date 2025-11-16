5dae14df80191aaf4840c583b240a6b1f21595d2
shanmugavasanr-risklit <cofounder@risklit.com>
2025-11-16 21:48:26 +0000

    auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import HeroSection from "@/components/HeroSection";
 import ScannerForm from "@/components/ScannerForm";
 import LandingPage from "@/components/LandingPage";
 import FreeReportsPromo from "@/components/FreeReportsPromo";
+import "../amplifyConfig";
 //import AlertSection from "@/components/AlertSection"; <AlertSection />
 // import Footer from "@/components/Footer"; <Footer />
 
